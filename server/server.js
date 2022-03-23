5657bdf21039efe6e9b220343d3fe23d2d1bf04d
ShanchaiZ <shanchai.zahid@gmail.com>
2022-03-23 02:21:48 -0400

    updated server/server.js with comments

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
+//Required Express server
 const express = require('express');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
 
+//The port 3001 in use
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+//Middleware
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
